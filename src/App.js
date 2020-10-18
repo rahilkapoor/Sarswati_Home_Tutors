7c13893d372bc302c2dcf173d2ae843b48aff97c
Vividh Bhardwaj <bhardwajvividh@gmail.com>
2020-10-18 14:14:31 +0530

    Carousel, Navbar and Pricing added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Jumbotron from "./components/Carousel";
+import Home from "./components/Home";
+import Price from "./components/Price";
 
 function App() {
-	return (
-		<div className="App">
-			<h1>Saraswati Home Tutors</h1>
-		</div>
-	);
+    return (
+        <div className="App">
+            <Router>
+                <Header />
+                <Switch>
+                    <Route path="/about">
+                        <h1>About Page</h1>
+                    </Route>
+                    <Route path="/services">
+                        <h1>Service Page</h1>
+                    </Route>
+                    <Route path="/pricing">
+                        <Price />
+                    </Route>
+                    <Route path="/">
+                        <Jumbotron />
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Services.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Services() {
+    return (
+        <div className="services">
+            {/* <Locations/> */}
+            {/* < Subjects/> */}
+            {/* <  /> */}
+        </div>
+    );
+}
+
+export default Services;
