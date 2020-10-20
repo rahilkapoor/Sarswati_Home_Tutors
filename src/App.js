e785e2eeecdc4e3023958251f6840e363c0523dc
Rahil Kapoor <rahilkapoor2000@gmail.com>
2020-10-20 10:31:13 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
 	return (
 		<div className="App">
+			<Navbar />
 			<h1>Saraswati Home Tutors</h1>
 		</div>
 	);
